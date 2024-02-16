be729650801ac41c2a1c300d4afbf71983f9e40b
Amy Bender <abender@anl.gov>
2024-02-16 09:08:51 -0600

    improved robustness of journal publication info processing

--- assets/js/publications.js
+++ assets/js/publications.js
@@ -6,9 +6,19 @@ function getRecentPubs()
             var title = data.hits.hits[jrecord].metadata.titles[0].title;
             var arxiv_text = 'arxiv:' + data.hits.hits[jrecord].metadata.arxiv_eprints[0].value;
             var arxiv_link = 'https://arxiv.org/abs/' + data.hits.hits[jrecord].metadata.arxiv_eprints[0].value;
-            if (data.hits.hits[jrecord].metadata.hasOwnProperty('publication_info')) {
+
+	    if (data.hits.hits[jrecord].metadata.hasOwnProperty('publication_info')) {
                 console.log(data.hits.hits[jrecord].metadata.publication_info);
-                var journal_name = data.hits.hits[jrecord].metadata.publication_info[0].journal_title;
+		for (kinfo = 0; kinfo < data.hits.hits[jrecord].metadata.publication_info.length; kinfo++){
+		    if (data.hits.hits[jrecord].metadata.publication_info[kinfo].hasOwnProperty('journal_title')){
+			var journal_name = data.hits.hits[jrecord].metadata.publication_info[kinfo].journal_title;
+			break;
+		    } else {
+			var journal_name ='';
+		    }
+		}
+
+		if (journal_name != ''){
                 journal_name = journal_name.replace(/\./g, '. ');
                 if (journal_name.substr(journal_name.length - 1) == ' ') {
                     journal_name = journal_name.slice(0, -1);
@@ -29,10 +39,12 @@ function getRecentPubs()
                     var journal_year = data.hits.hits[jrecord].metadata.publication_info[0].year;
                     journal_text += ', (' + journal_year + ')';
                 }
-            }
-            else {
+		}
+		                
+            } else {
                 var journal_text = '';
             }
+	    	   
             $('#publications').append('<li><i>' + title + '</i>, <a href=' + arxiv_link + '>' + arxiv_text + '</a> ' + journal_text + '</li>')
         }
         
